f8e53accc0a49bdfd76541c4a6107759849e14c7
mrprince143 <uniq754386@gmail.com>
2025-11-04 21:30:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
 
 // Serve the index.html file
 app.get('/', function (req, res) {
